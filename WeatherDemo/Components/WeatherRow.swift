86910f845c2f3c53e7d3c2dccae2475af7710424
aliHuamanMediabyte <114945139+aliHuamanMediabyte@users.noreply.github.com>
2024-05-26 12:34:10 -0500

    if applied this will add rest of weather ui and details

---
+++ WeatherDemo/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherDemo
+//
+//  Created by Antony Huaman Alikhan on 26/05/24.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
